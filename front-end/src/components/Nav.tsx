85a86543b4796774f63a12cd4f48bc7bf8375d6b
nam anh tran van <namanh022@gmail.com>
2023-02-21 04:45:16 +0200

    change toggle

--- front-end/src/components/Nav.tsx
+++ front-end/src/components/Nav.tsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogout, setModeLightDark } from "../state";
 
 type Props = {};
 
 const Nav = (props: Props) => {
   const [mode, setMode] = useState("light");
   const [isOpen, setIsOpen] = useState(false);
-
+  const { user } = useSelector((state: any) => state);
+  const dispatch = useDispatch();
   const toggleLightDark = () => {
     setMode(mode === "light" ? "dark" : "light");
+    dispatch(setModeLightDark());
+  };
+
+  const handleLogout = () => {
+    dispatch(setLogout());
   };
 
   return (
-    <div className={`container-fluid bg-${mode}`}>
-      <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode} p-3`}>
+    <div
+      className={`container-fluid bg-${mode === "light" ? "white" : "dark"} `}
+      data-bs-theme={`${mode}`}
+    >
+      <nav className={`navbar navbar-expand-lg p-3`}>
         <a className={`navbar-brand text-weight-bold fs-3`} href="#">
-          Campsite
+          Camping Site
         </a>
         <button
           className="navbar-toggler"
@@ -36,7 +47,7 @@ const Nav = (props: Props) => {
         >
           <ul className="navbar-nav text-center">
             <li className="nav-item">
-              <a className="nav-link fs-6" href="#">
+              <a className="nav-link fs-6 active" href="/">
                 Home
               </a>
             </li>
@@ -46,29 +57,51 @@ const Nav = (props: Props) => {
               </a>
             </li>
           </ul>
-          <ul className="navbar-nav text-center">
-            <li className="nav-item">
-              <a className="nav-link fs-6" href="#">
-                Register
-              </a>
-            </li>
-            <li className="nav-item ">
-              <a className="nav-link fs-6" href="#">
-                Login
-              </a>
-            </li>
-            <li>
-              {mode === "light" ? (
-                <button className="btn" onClick={toggleLightDark}>
-                  <MdDarkMode />
-                </button>
-              ) : (
-                <button className="btn" onClick={toggleLightDark}>
-                  <MdLightMode className={`text-white`} />
-                </button>
-              )}
-            </li>
-          </ul>
+
+          {user ? (
+            <ul className="navbar-nav text-center">
+              <li className="nav-item">
+                <a className="nav-link fs-6" href="/" onClick={handleLogout}>
+                  Logout
+                </a>
+              </li>
+              <li>
+                {mode === "light" ? (
+                  <button className="btn" onClick={toggleLightDark}>
+                    <MdDarkMode />
+                  </button>
+                ) : (
+                  <button className="btn" onClick={toggleLightDark}>
+                    <MdLightMode className="text-white" />
+                  </button>
+                )}
+              </li>
+            </ul>
+          ) : (
+            <ul className="navbar-nav text-center">
+              <li className="nav-item">
+                <a className="nav-link fs-6" href="/register">
+                  Register
+                </a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link fs-6" href="/">
+                  Login
+                </a>
+              </li>
+              <li>
+                {mode === "light" ? (
+                  <button className="btn" onClick={toggleLightDark}>
+                    <MdDarkMode />
+                  </button>
+                ) : (
+                  <button className="btn" onClick={toggleLightDark}>
+                    <MdLightMode className="text-white" />
+                  </button>
+                )}
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
     </div>
